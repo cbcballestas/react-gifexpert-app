02cecbf1bd82d139e4a5215ef4411aa63c1cda62
Carlos Alberto Ballestas <cbcballestas@gmail.com>
2020-07-17 23:03:45 -0500

    docs agregado

---
+++ docs/precache-manifest.7de4f48341bc71b2e83fac0dfe905383.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a939a7e583bbd459241b216781eb1c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c026f5421b90c7b67615",
+    "url": "/static/js/main.3cad88be.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
